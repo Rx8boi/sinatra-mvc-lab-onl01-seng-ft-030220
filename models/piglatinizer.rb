8e02155e32db542ab6a535d731130015a8bba965
Charles Butler <cdbutle@outlook.com>
2020-05-02 00:19:50 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+
+class PigLatinizer
+
+  def piglatinizer(word)
+    exclude = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+     vowels = ["a", "e", "i", "o", "u"]
+
+     if exclude.include?(word)
+        word
+      elsif vowels.include? word[0]
+        word << "ay"
+        else
+          consonants = ""
+        while !vowels.include?(word[0])
+          consonants << word[0]
+          word = word.split("")[1..-1].join
+        end
+        word + consonants + 'ay'
+      end
+end
+
+  def to_pig_latin (sentence)
+    sentence.split.map{|word| piglatinize(word)}.join(' ')
+  end
+end
