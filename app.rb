d9c54bd8b162a6d72ddc641aa38d91bd4b40dfe7
Charles Butler <cdbutle@outlook.com>
2020-05-02 00:21:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,12 +5,12 @@ class App < Sinatra::Base
   get '/' do
       erb :user_input
     end
-  
+
     post '/piglatinize' do
       user_word = params["user_phrase"]
       word = PigLatinizer.new
       @pig_latinized_word = word.to_pig_latin(user_word)
-  
+
       erb :pit_latin
     end
   end
